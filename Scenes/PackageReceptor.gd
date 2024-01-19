664ba794736754381cc8bb7800d60462ba778eb1
GsX <156434433+GsX0789@users.noreply.github.com>
2024-01-18 22:09:46 -0300

    PackageReceptor Update
    Now the player gets a x amount of score when dropping the create in the right place

--- Scenes/PackageReceptor.gd
+++ Scenes/PackageReceptor.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
-const xSpeed = 3.5
-const PACKAGE_GP_NAME = "Package" 
+const xSpeed = 10.5
+const PACKAGE_GP_NAME = "Package"
+var hitScore : int = 20 
 
 func _process(delta):
 	position.x -= xSpeed
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group(PACKAGE_GP_NAME):
+		Autoload.countScore(hitScore)
 		body.DestroyPackage()
 
